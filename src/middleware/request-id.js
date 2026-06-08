3114dfa588f70b041f40e4895ae86e24f031b407
CredoDivaldo <credolopes@icloud.com>
2026-06-08 10:42:45 +0100

    chore: remove legacy express tree and tests [Story 8.5]

--- src/middleware/request-id.js
+++
@@ -1,9 +0,0 @@
-const crypto = require("crypto");
-
-function requestIdMiddleware(req, res, next) {
-  req.id = crypto.randomUUID().slice(0, 8);
-  res.setHeader("X-Request-Id", req.id);
-  next();
-}
-
-module.exports = { requestIdMiddleware };
